666afd62d428e8ce4d439aa684fe615c72910eb1
Javier Gómez <137917002+RundelCode@users.noreply.github.com>
2025-12-22 22:19:15 -0500

    Main

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,12 @@ async function bootstrap() {
     }),
   );
 
-  // Configuración de CORS
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
